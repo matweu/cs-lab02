f814e52eae080f8958ca30e2820102a99df838eb
Bob(ZhukovMV) <zhuk31@list.ru>
2022-03-13 23:03:43 +0300

    code: работа boba(минимум)

--- main.cpp
+++ main.cpp
@@ -11,5 +11,9 @@ int main()
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    if(a<b)
+        cout<<a;
+    else
+        cout<<b;
 
 }
